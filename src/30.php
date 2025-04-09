39f3230bfdd54654d9296d86ce9fd865825be878
labauvestastny717 <labauvestastny717@gmail.com>
2025-04-09 06:56:32 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,8 @@
+<?php
+// Assuming $username is the value of your GitHub username
+$username = 'your_username_here'; // Replace with your actual GitHub username
+
+// Your PHP code goes here
+echo "Hello, !Welcome to GitHub School Project! <br> Please follow the instructions below:</br><br>";
+
+// Code for user login and access
