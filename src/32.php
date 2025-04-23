7b9b12914019cd4d0e0291ed954b5c470f161c37
labauvestastny717 <labauvestastny717@gmail.com>
2025-04-23 10:40:29 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,16 @@
+<?php
+$fname = "example.txt";
+$age = 18;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Example</title>
+</head>
+<body>
+	<h1>Welcome to PHP Example!</h1>
+	<p>Your name is: <?php echo $fname; ?></p>
+	<p>Your age is: <?php echo $age; ?></p>
+</body>
+</html>
